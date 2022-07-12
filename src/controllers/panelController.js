085a3f9b5029aac465b4776522f3a2aee29e3629
Dr4x-k <jabe.4124@gmail.com>
2022-07-11 21:05:22 -0700

    Actualizar, borrar y tabla de usuarios funcionando

--- src/controllers/panelController.js
+++ src/controllers/panelController.js
@@ -29,7 +29,11 @@ panelController.panerEmpData = (req, res) => {
     })
 }
 
-panelController.createNewCliente = (req, res) => {
+panelController.panelCreate = (req, res) => {
+    res.render('panel/panelCreate')
+}
+
+panelController.createNewEmpleado = (req, res) => {
     // const idUsuario = 0;
     const nombres = req.body.name,
             apellidoPaterno = req.body.apPaterno,
@@ -41,26 +45,29 @@ panelController.createNewCliente = (req, res) => {
             // codigoPostal = req.body.cp,
             usuario = req.body.user, 
             contrasena = req.body.pssw
-            fk_rol = req.body.rol;
+            fk_rol = req.body.typeUser;
+        
+            // if (req.body.typeUser == 'Administrador')
 
-    conexion.query('insert into usuario (nombres, apellidoPaterno, apellidoMaterno, telefono, usuario, contrasena) set ?', { nombres, apellidoPaterno, apellidoMaterno, telefono, usuario, contrasena, fk_rol }, (err, empleados) => {
+    conexion.query('insert into usuario set ?', { nombres, apellidoPaterno, apellidoMaterno, telefono, usuario, contrasena, fk_rol }, (err, empleados) => {
         if (err) {
             // res.json(err)
             throw err;
         } 
-        res.redirect('panel/panelEmpleados')
+        res.redirect('/panelE')
     })
-        // console.log(data)
+        // console.log(fk_rol)
 }
 
 panelController.edit = (req, res) => {
     const idUsuario = req.params.idUsuario;
 
-    conexion.query('select * from usuario where idUsuario = ?', {idUsuario}, (err, results) => {
+    conexion.query('select * from usuario where idUsuario = ?', [idUsuario], (err, results) => {
         if (err) {
             throw err;
         } else {
-            res.render('panel/panelUpdate', { data:results[0] });
+            res.render('panel/panelUpdate', { data : results[0] });
+            // console.log({data:results})
         }
     })
 }
@@ -73,9 +80,9 @@ panelController.updateEmpleados = (req, res) => {
             telefono = req.body.cel,
             usuario = req.body.user, 
             contrasena = req.body.pssw
-            fk_rol = req.body.rol;
+            fk_rol = req.body.typeUser;
     
-    conexion.query('update usuario set ? where idUsuario = ?', [ { nombres, apellidoPaterno, apellidoMaterno, telefono, usuario, contrasena }, idUsuario], (err, empleados) => {
+    conexion.query('update usuario set ? where idUsuario = ?', [ { nombres, apellidoPaterno, apellidoMaterno, telefono, usuario, contrasena, fk_rol }, idUsuario], (err, empleados) => {
         if (err) {
             throw err;
         } else {
--- src/routes/dashRoutes.js
+++ src/routes/dashRoutes.js
@@ -4,12 +4,9 @@ const express = require('express'),
         loginController = require('../controllers/loginController'),
         signupController = require('../controllers/signupController'),
         panelController = require('../controllers/panelController'),
-// const { routes } = require('../app')
         conexion = require('../database/connection'),
 
 // ruta controladores
-// import controller from '../controllers/controller.js'
-
 routes = express.Router()
 
 routes.get('/', (req, res) => {
@@ -22,34 +19,13 @@ routes.get('/login', loginController.list)
 routes.get('/home', panelController.panelHome)
 routes.get('/panelE', panelController.panelEmp)
 routes.get('/data', panelController.panerEmpData)
+routes.get('/panelCreate', panelController.panelCreate)
+routes.post('/save', panelController.createNewEmpleado)
 routes.get('/update/:idUsuario', panelController.edit)
 routes.get('/delete/:idUsuario', panelController.deleteEmpleados)
 routes.post('/update', panelController.updateEmpleados)
 
 routes.get('/signup', signupController.view)
 routes.post('/save', signupController.save)
-
-
-/* routes.get('/panelE', (req, res) => {
-    conexion.query('select idUsuario, nombres, apellidoPaterno, apellidoMaterno, usuario from usuario', (err, results) => {
-        if (err) {
-            res.json(err)
-        } else {
-            res.render('controlPanel/controlPanelEmpleados', { data:results })
-            console.log(results)
-        }
-    })
-})
-
-routes.get('/data', (req, res) => {
-    conexion.query('select idUsuario, nombres, apellidoPaterno, apellidoMaterno, usuario from usuario', (err, results) => {
-        if (err) {
-            res.json(err)
-        } else {
-            data = JSON.stringify(results)
-            res.send
-        }
-    })
-}) */
 
 module.exports = routes
